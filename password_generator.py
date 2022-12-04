39aaef53f0e86355c7a84fd1af3bc8dbf1d6a31a
wret8385 <87554209+wret8385@users.noreply.github.com>
2022-12-04 13:38:23 -0700

    Add files via upload

---
+++ password_generator.py
@@ -0,0 +1,25 @@
+import string
+import random
+
+characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
+
+def generate_random_password():
+	## length of password from the user
+	length = int(input("Enter password length: "))
+
+	## shuffling the characters
+	random.shuffle(characters)
+	
+	## picking random characters from the list
+	password = []
+	for i in range(length):
+		password.append(random.choice(characters))
+
+	## shuffling the resultant password
+	random.shuffle(password)
+
+	## converting the list to string
+	## printing the list
+	print("".join(password))
+
+generate_random_password()
